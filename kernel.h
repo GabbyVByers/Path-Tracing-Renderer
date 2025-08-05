0c467c7066dd20eccfa0522549bb22e04c928094
Gabby <GabbyVByers@gmail.com>
2025-08-04 23:02:32 -0700

    rollback to here if my next attempt at direct light sampling fails

--- kernel.h
+++ kernel.h
@@ -5,32 +5,6 @@
 #include "random.h"
 #include <cfloat>
 
-__device__ inline bool cheap_shadow_test(const Ray& ray, const World& world)
-{
-    for (int i = 0; i < world.num_spheres; i++)
-    {
-        Vec3 V = ray.origin - world.device_spheres[i].position;
-        float a = dot(ray.direction, ray.direction);
-        float b = 2.0f * dot(V, ray.direction);
-        float c = dot(V, V) - (world.device_spheres[i].radius * world.device_spheres[i].radius);
-
-        float discriminant = (b * b) - (4.0f * a * c);
-        if (discriminant <= 0.0f)
-            continue;
-
-        float t1 = ((-b) + sqrt(discriminant)) / (2.0f * a);
-        float t2 = ((-b) - sqrt(discriminant)) / (2.0f * a);
-        float t = fmin(t1, t2);
-
-        if (t <= 0.0f)
-            continue;
-
-        return true;
-    }
-
-    return false;
-}
-
 __device__ inline Hit_info ray_spheres_intersection(const Ray& ray, const World& world)
 {
     Hit_info info;
@@ -70,67 +44,23 @@ __device__ inline Hit_info ray_spheres_intersection(const Ray& ray, const World&
     return info;
 }
 
-__device__ inline Vec3 lambertian_shading(const Hit_info& info, const World& world)
-{
-    Vec3 color = info.hit_color;
-
-    float cos_weight = dot(info.hit_normal, world.light_direction);
-
-    if (cos_weight <= 0.0f)
-        cos_weight = 0.0f;
-
-    Ray ray;
-    ray.origin = info.hit_location;
-    ray.direction = world.light_direction;
-
-    if (cheap_shadow_test(ray, world))
-        cos_weight = 0.0f;
-
-    float dimming_factor = world.ambient_brightness + ((1.0f - world.ambient_brightness) * cos_weight);
-
-    return color * dimming_factor;
-}
-
 __device__ inline Vec3 environment_light(const Ray& ray, const World& world)
 {
-    Vec3 sun_color;
-    sun_color = world.sun_intensity;
-    Vec3 sky_zenith = rgb(47, 175, 222);
-    Vec3 sky_horizon = rgb(255, 255, 255);
-    Vec3 ground_color = rgb(130, 126, 122);
-
-    float horizon_zenith_gradient = pow(smoothstep(0.0f, 0.4f, ray.direction.y), 0.35);
-
-
-    float cosine_sun = dot(ray.direction, world.light_direction);
-
-    float sun_size = 0.05f;
-    float corona_width = 0.1f;
-
-    float true_sun_mask = 0.0f;
-    if (cosine_sun > (1.0f - sun_size))
-        true_sun_mask = 1.0f;
-
-
+    
+    
+    Vec3 sky_blue = rgb(55, 107, 171);
+    Vec3 ground_color = rgb(94, 91, 88);
+    
     if (ray.direction.y < 0.0f)
         return ground_color;
-
-    Vec3 sky_color = lerp_between_vectors(sky_horizon, sky_zenith, horizon_zenith_gradient) + (sun_color * true_sun_mask);
-    return sky_color;
-}
-
-__device__ inline Vec3 sky_light(const Ray& ray, const World& world)
-{
-    Vec3 white = rgb(255, 255, 255);
-    //Vec3 sky_blue = rgb();
-    //Vec3 ground_color = rgb();
-
-    if (dot(ray.direction, world.light_direction) > 0.9f)
+    
+    if (dot(ray.direction, world.light_direction) > 0.85f)
     {
+        Vec3 white = rgb(255, 255, 255);
         return white * world.sun_intensity;
     }
     
-    return { 0.0f, 0.0f, 0.0f };
+    return sky_blue;
 }
 
 __device__ inline Vec3 calculate_incoming_light(Ray ray, Thread& thread, const World& world)
