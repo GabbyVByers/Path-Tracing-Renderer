5f2e38ef31200ef01fd3d99ff0a5b6be6942d356
Gabby <gabbyvbyers@gmail.com>
2025-08-14 19:25:18 -0700

    Update sharedarray.h

--- sharedarray.h
+++ sharedarray.h
@@ -7,7 +7,7 @@
 	A utility for managing an array of elements between the Host and Device
 	The Host is responsible for adding or removing elements from the array.
 	The Device need only concern herself with reading or writing data to the existing array.
-	Call the appropriate update function if the host or device updates either of their respective arrays.
+	Call the appropriate update function (from the host) if the host or device updates either of their respective arrays.
 */
 
 template<typename type>
